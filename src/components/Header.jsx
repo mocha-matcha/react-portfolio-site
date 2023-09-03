579a8c3c1c04bb4e6dd3971e2e607e625c0876f8
mocha <mocha@localhost.localdomain>
2023-09-02 20:33:00 -0500

    init work.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (<>
+        <header>
+            <a href="">About</a>
+            <a href="">Home</a>
+            <a href="">Projects</a>
+            <a href="">Contact</a>
+        </header>
+    </>);
+}
